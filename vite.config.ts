22221c5887b053997fdb9d62c5409ed39ad3e327
Praveensharmaa1 <praveen.sharmaa1@gmail.com>
2025-06-28 14:24:32 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: {
+    outDir: 'dist', // 🟢 Required by Vercel
   },
 });

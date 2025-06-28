c4cdec3eec2149eda278be53a57d553477d87579
Praveensharmaa1 <praveen.sharmaa1@gmail.com>
2025-06-28 15:14:17 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
+import Home from './pages/Home';
+
 function App() {
-  return (
-    <div className="text-center p-10">
-      <h1 className="text-3xl font-bold text-blue-600">BestFit-Bolt is Live!</h1>
-      <p className="text-lg mt-2">This is your Vite + React + TypeScript app.</p>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
